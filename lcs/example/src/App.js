0f5db58fb1e5f8a04ab6de40b72ccc3cbd2f2f38
Ryan Goldstein <ryanisaacg@gmail.com>
2019-05-07 22:52:33 -0400

    Attempt to add a glow button

--- lcs/example/src/App.js
+++ lcs/example/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { Button } from "lcs";
+import { Button, GlowButton } from "lcs";
+
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Button>Modern React component module</Button>
+                <Button> Modern React component module </Button>
+                <GlowButton />
             </div>
         )
     }
