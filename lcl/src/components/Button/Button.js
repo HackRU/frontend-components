b949d0d13a73a81b08492ca8641e83d5b7707412
Ryan Goldstein <ryanisaacg@gmail.com>
2019-06-15 10:39:58 -0700

    Add ESLint configuration and commands
    Run 'npm run lint' to get linting errors and 'npm run fix' to
    automatically fix errors that ESLint can fix for you. Any editor with
    lint support should detect the config and use it for errors.

--- lcl/src/components/Button/Button.js
+++ lcl/src/components/Button/Button.js
@@ -1,15 +1,23 @@
-import React, { Component } from "react";
-import { Button as BsButton } from "reactstrap";
-import PropTypes from "prop-types";
+import React from 'react';
+import { Button as BsButton } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 const Button = ({ className, children, color, onClick, style }) => (
     <BsButton
-        className={`lcl-btn-${(color) ? color : "primary"} ${className}`}
-        color={(color) ? color : "primary"}
+        className={`lcl-btn-${(color) ? color : 'primary'} ${className}`}
+        color={(color) ? color : 'primary'}
         onClick={onClick}
         style={style}>
         {children}
     </BsButton>
-)
+);
+
+Button.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+    style: PropTypes.object,
+};
 
 export default Button;
--- lcl/src/components/GlowButton/GlowButton.js
+++ lcl/src/components/GlowButton/GlowButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button } from "reactstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 
 const GlowButton = ({ href, icon, text }) => (
     <Button href={href} className="glow-button" size="lg" color="info" outline>
@@ -8,6 +9,12 @@ const GlowButton = ({ href, icon, text }) => (
         <br/>
         { text }
     </Button>
-)
+);
+
+GlowButton.propTypes = {
+    href: PropTypes.string,
+    icon: PropTypes.string,
+    text: PropTypes.node,
+};
 
 export default GlowButton;
