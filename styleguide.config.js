e3b914136ffde725131a32a2b3d7f59d79b93e63
Shivan Modha <shivanmodha@gmail.com>
2019-06-30 19:19:00 -0400

    Updated Webpack configuration for Production Build

--- styleguide.config.js
+++ styleguide.config.js
@@ -7,5 +7,12 @@ module.exports = {
     components: "./src/components/**/*.js", // Regex for our component directory layout
     require: [
         path.join(__dirname, "dist/lcl.css"), // Static css import of LCL
-    ]
+    ],
+    dangerouslyUpdateWebpackConfig(webpackConfig, env) {
+        webpackConfig.output = {
+            ...webpackConfig.output,
+            publicPath: process.env.PUBLIC_URL || "/"
+        };
+        return webpackConfig;
+    }
 };
